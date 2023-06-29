e5211b8b6898d79e5e14dc0f24b3721dd00c1850
carlos <carlos@gmail.com>
2023-06-29 10:56:50 -0300

    projeto

--- src/main/java/com/example/estacionamento/entities/Department.java
+++ src/main/java/com/example/estacionamento/entities/Department.java
@@ -10,17 +10,22 @@ public class Department {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     //ForeignKey
-    private long id;
+    private Long id;
     //long tipo de dado para numeros grandes
 
     private String name;
 
 
-    public long getId() {
+    public Department(){
+
+    }
+
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
